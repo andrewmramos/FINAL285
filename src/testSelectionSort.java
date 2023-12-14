13e4a9f065b56a23bbda657fb1b05d3007e49dbb
Andrew <andrewmramos7@gmail.com>
2023-12-14 15:30:18 -0500

    inital commit

---
+++ src/testSelectionSort.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionSort {
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+	}
+	public testSelectionSort() {
+	}
+	
+	public void testPositive(){
+		
+		
+	}
+	
+	public void testNegative(){
+		
+		
+	}
+	
+	public void testMixed(){
+		
+		
+	}
+}
